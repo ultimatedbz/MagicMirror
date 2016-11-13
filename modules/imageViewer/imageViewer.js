753e3e7f8062fd32c8de64963c43846474a54be7
Ryan Lawler <ryanlawler@berkeley.edu>
2016-11-13 06:10:26 -0800

    added imgsrc3

--- modules/imageViewer/imageViewer.js
+++ modules/imageViewer/imageViewer.js
@@ -14,14 +14,14 @@ Module.register("imageViewer",{
 		text: "Hello World!",
     imgsrc: "http://vh1.mtvnimages.com/uri/mgid:file:http:shared:vh1.com/news/wp-content/uploads/2015/08/thanks-1438616370.gif",
     imgsrc2: "http://66.media.tumblr.com/tumblr_lr2ysgGcMX1qhz1tdo3_r3_250.gif",
+    imgsrc3: "http://67.media.tumblr.com/c501c17987afa726df6936464d5da3e7/tumblr_n8i1jcXggZ1sqtzdpo2_r1_500.gif"
 	},
 
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
-	//	wrapper.innerHTML = this.config.text;
     var elem = document.createElement("img");
-    elem.src = this.config.imgsrc2;
+    elem.src = this.config.imgsrc3;
     elem.height = 200;
     elem.width = 200;
     wrapper.appendChild(elem);
